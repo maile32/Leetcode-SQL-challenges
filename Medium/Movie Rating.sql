7566e9fd5ecdc906ac2b71ee66e91fcf5a3906d9
maile32 <108832327+maile32@users.noreply.github.com>
2022-07-25 17:31:04 +0100

    Create Movie Rating.sql

---
+++ Medium/Movie Rating.sql
@@ -0,0 +1,112 @@
+-- Question 59
+-- Table: Movies
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | title         | varchar |
+-- +---------------+---------+
+-- movie_id is the primary key for this table.
+-- title is the name of the movie.
+-- Table: Users
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | user_id       | int     |
+-- | name          | varchar |
+-- +---------------+---------+
+-- user_id is the primary key for this table.
+-- Table: Movie_Rating
+
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | movie_id      | int     |
+-- | user_id       | int     |
+-- | rating        | int     |
+-- | created_at    | date    |
+-- +---------------+---------+
+-- (movie_id, user_id) is the primary key for this table.
+-- This table contains the rating of a movie by a user in their review.
+-- created_at is the user's review date. 
+ 
+
+-- Write the following SQL query:
+
+-- Find the name of the user who has rated the greatest number of the movies.
+-- In case of a tie, return lexicographically smaller user name.
+
+-- Find the movie name with the highest average rating in February 2020.
+-- In case of a tie, return lexicographically smaller movie name.
+
+-- Query is returned in 2 rows, the query result format is in the folowing example:
+
+-- Movies table:
+-- +-------------+--------------+
+-- | movie_id    |  title       |
+-- +-------------+--------------+
+-- | 1           | Avengers     |
+-- | 2           | Frozen 2     |
+-- | 3           | Joker        |
+-- +-------------+--------------+
+
+-- Users table:
+-- +-------------+--------------+
+-- | user_id     |  name        |
+-- +-------------+--------------+
+-- | 1           | Daniel       |
+-- | 2           | Monica       |
+-- | 3           | Maria        |
+-- | 4           | James        |
+-- +-------------+--------------+
+
+-- Movie_Rating table:
+-- +-------------+--------------+--------------+-------------+
+-- | movie_id    | user_id      | rating       | created_at  |
+-- +-------------+--------------+--------------+-------------+
+-- | 1           | 1            | 3            | 2020-01-12  |
+-- | 1           | 2            | 4            | 2020-02-11  |
+-- | 1           | 3            | 2            | 2020-02-12  |
+-- | 1           | 4            | 1            | 2020-01-01  |
+-- | 2           | 1            | 5            | 2020-02-17  | 
+-- | 2           | 2            | 2            | 2020-02-01  | 
+-- | 2           | 3            | 2            | 2020-03-01  |
+-- | 3           | 1            | 3            | 2020-02-22  | 
+-- | 3           | 2            | 4            | 2020-02-25  | 
+-- +-------------+--------------+--------------+-------------+
+
+-- Result table:
+-- +--------------+
+-- | results      |
+-- +--------------+
+-- | Daniel       |
+-- | Frozen 2     |
+-- +--------------+
+
+-- Daniel and Maria have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
+-- Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.
+
+-- Solution
+with t1 as
+(select name, rank()over(order by u1 desc, name) as ru1
+from (select name, count(mr.user_id) over(partition by mr.user_id) as u1
+from movie_rating as mr
+left join users
+on mr.user_id = users.user_id
+group by name) as t2),
+
+t3 as
+(select title, rank() over(order by m1 desc, title) as rm1
+from (select title, avg(rating) over(partition by mr.movie_id) as m1
+from movie_rating as mr
+left join movies
+on mr.movie_id = movies.movie_id
+where month(created_at) = 2
+group by title) as t4)
+
+
+select name from t1 where ru1 = 1
+union
+select title as name from t3 where rm1 = 1;
